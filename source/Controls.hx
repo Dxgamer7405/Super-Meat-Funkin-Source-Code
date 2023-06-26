9858292a4e0457401524fdca3ea5ca50246093af
Dxgamer7405 <95672691+Dxgamer7405@users.noreply.github.com>
2023-06-26 06:38:33 -0400

    ?????????

--- source/Controls.hx
+++ source/Controls.hx
@@ -258,14 +258,14 @@ class Controls extends FlxActionSet {
 
 	public function addButtonNOTES(action:FlxActionDigital, button:FlxButton, state:FlxInputState)
 	{
-		var input:FlxActionInputDigitalIFlxInput = new FlxActionInputDigitalIFlxInput(button, state);
+		var input:FlxActionInputDigital = new FlxActionInputDigital(button, state);
 		trackedInputsNOTES.push(input);
 		action.add(input);
 	}
 
 	public function addButtonUI(action:FlxActionDigital, button:FlxButton, state:FlxInputState)
 	{
-		var input:FlxActionInputDigitalIFlxInput = new FlxActionInputDigitalIFlxInput(button, state);
+		var input:FlxActionInputDigital = new FlxActionInputDigital(button, state);
 		trackedInputsUI.push(input);
 		action.add(input);
 	}
